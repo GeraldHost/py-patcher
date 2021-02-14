4d0c4b614ac624e80c7b1e30cf1c1f683c2e0262
Gerald <me@jacobford.co.uk>
2021-02-14 12:26:08 +0000

    plan for finding and patching

--- patcher.py
+++ patcher.py
@@ -1,9 +1,18 @@
 from utils import hexstr_to_hex, hexstr_to_int
+from objdump import Section
 
 class Patcher:
     def __init__(self, binary, offset):
         self.offset = offset
         self.binary = binary
+
+    def scanbackwards(self, section):
+        # scan backwards until we get to to start of the section
+        # if we come across and jumps patch them through
+        # once we are at the start of the section scan the entire
+        # binary looking for calls to the section
+        # repeat
+        return 0
 
     def findoffset(self):
         section_addrs = self.binary.keys()
@@ -15,10 +24,12 @@ class Patcher:
                 print(f"[*] Offset found: {self.offset}")
                 key = list(section_addrs)[i-1]
                 return self.binary[key]
-        print("[*] Offset not found")
 
 def patch(binary, offset):
     # find address in the binary to check it exists
     # walk backwards and patch any jumps
     patcher = Patcher(binary, offset)
     section = patcher.findoffset()
+
+    if not isinstance(section, Section):
+        print("[*] Offset not found")
